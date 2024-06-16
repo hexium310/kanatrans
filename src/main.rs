902839e7ea26936b55f9d66a860e22e1ef4099ae
Hexin <hexium310@gmail.com>
2024-06-17 06:02:41 +0900

    Fix panic when existing application

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,8 @@ async fn main() {
     let arpabet_service = ArpabetService::new(LexLookup::new(LexLookupExecutor::new()));
     let katakana_service = KatakanaService::new(ConversionTable::new(KatakanaConverter::new()));
 
-    match infrastructure::router::start(arpabet_service, katakana_service).await {
-        Ok(_) => todo!(),
-        Err(err) => {
-            tracing::error!("failed to serve:\n{err:?}");
-            exit(1);
-        },
-    }
+    if let Err(err) = infrastructure::router::start(arpabet_service, katakana_service).await {
+        tracing::error!("failed to serve:\n{err:?}");
+        exit(1);
+    };
 }
