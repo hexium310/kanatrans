2b1f0678155f8ea0f7907943e91f0ad205dea4d7
Hexin <hexium310@gmail.com>
2024-07-05 19:47:18 +0900

    Stop logging filename and line number

--- crates/kanatrans-web-app/src/main.rs
+++ crates/kanatrans-web-app/src/main.rs
@@ -17,8 +17,6 @@ async fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .with_timer(local_timer)
-        .with_file(true)
-        .with_line_number(true)
         .init();
 
     let arpabet_service = ArpabetService::new(LexLookup::new(LexLookupExecutor));
