01757306459ede85c594d29429ab08acb02d9e04
Hexin <hexium310@gmail.com>
2025-02-06 17:59:08 +0900

    Remove router configuration in server

--- crates/service/src/routing.rs
+++ crates/service/src/routing.rs
@@ -1,17 +1,20 @@
 use std::sync::Arc;
 
-use adapter::processor::{conversion_table::ConversionTable, lex_lookup::LexLookup};
 use axum::Router;
 
 use crate::{
-    arpabet::{self, ArpabetService},
-    katakana::{self, KatakanaService},
+    arpabet::{self, ArpabetServiceInterface},
+    katakana::{self, KatakanaServiceInterface},
 };
 
-pub async fn route() -> Router {
-    let arpabet_service = ArpabetService::<LexLookup>::default();
-    let katakana_service = KatakanaService::<ConversionTable>::default();
-
+pub fn router<ArpabetService, KatakanaService>(
+    arpabet_service: ArpabetService,
+    katakana_service: KatakanaService,
+) -> Router
+where
+    ArpabetService: ArpabetServiceInterface,
+    KatakanaService: KatakanaServiceInterface,
+{
     let arpabet = Router::new()
         .route("/{word}", axum::routing::get(arpabet::handle))
         .with_state(Arc::new(arpabet_service));
