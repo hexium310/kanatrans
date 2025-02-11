5516d5bfff6e734346720d296681eda894794b36
Hexin <hexium310@gmail.com>
2025-02-12 08:21:38 +0900

    wip test [skip ci]

--- crates/service/src/arpabet.rs
+++ crates/service/src/arpabet.rs
@@ -21,6 +21,7 @@ pub struct ArpabetService<Processor> {
     transcriber: Processor,
 }
 
+#[cfg_attr(test, mockall::automock)]
 pub trait ArpabetServiceInterface: Send + Sync + 'static {
     fn get(&self, word: String) -> impl Future<Output = Result<Arpabet, ServiceError>> + Send;
 }
