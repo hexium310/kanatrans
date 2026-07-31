86c8e1cbe30b58d37ef6bb9b76f168a4cdb662d5
Hexin <hexium310@gmail.com>
2026-07-31 19:36:49 +0900

    Allow clippy::all and unnecessary_transmutes in generated `bindings.rs`

--- crates/flite-sys/src/lib.rs
+++ crates/flite-sys/src/lib.rs
@@ -1,8 +1,10 @@
 #![allow(
+    clippy::all,
     improper_ctypes,
     non_camel_case_types,
     non_snake_case,
     non_upper_case_globals,
+    unnecessary_transmutes,
     unused_imports
 )]
 
